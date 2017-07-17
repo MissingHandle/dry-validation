f640c70315ba6e223b4ba5df4fb5039eea1c22f7
Gabe Saravia <missinghandle@users.noreply.github.com>
2017-07-17 10:35:10 -0500

    add failing test

---
+++ spec/integration/schema/array_as_attribute_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe Dry::Validation::Schema, 'for an array' do
+  context 'When a schema has a property that is an array' do
+    subject(:schema) do
+      array_schema = Dry::Validation.Schema do
+        each do
+          schema do
+            required(:prefix).filled
+            required(:value).filled
+          end
+        end
+      end
+
+      Dry::Validation.Schema do
+        required(:some_array_prop).schema(array_schema)
+      end
+    end
+
+    let(:valid_data) {
+      {
+        some_array_prop: [{ prefix: 1, value: 123 }, { prefix: 2, value: 456 }]
+      }
+    }
+    let(:invalid_data) {
+      {
+        some_array_prop: [{ prefix: 1, value: nil }, { prefix: nil, value: 456 }]
+      }
+    }
+
+    it 'valid data results in success' do
+      expect(schema.(valid_data)).to be_success
+    end
+
+    it 'valid data results in no messages' do
+      expect(schema.(valid_data).messages).to be_empty
+    end
+
+    it 'invalid data results in expected messages' do
+      result = schema.(invalid_data)
+
+      expect(result.messages).to eql(
+        some_array_prop: {
+          0 => { value: ["must be filled"] },
+          1 => { prefix: ["must be filled"] }
+        }
+      )
+    end
+  end
+end
